8eec30fb463e45f43003151d699e1713fcf2ed31
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-04 15:22:35 -0600

    Completed built_in_methods.rb & final_practice.rb

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,33 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# The include? method is called on the string object "Hello World"
+# One argument is passed; include? has one clear job which is used to see whether or
+# not the string argument passed is a substring of the object on which it is called.
+# include? returns a boolean value, in this case it would return true.
 "Hello World".include?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# One argument is passed; end_with? has one clear job which is used to check if the string
+# it is called on ends with the suffix passed in the argument.
+# The return value is a boolean, and in this case it would return false.
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# One argument is passed; end_with? has one clear job which is used to check if the string
+# it is called on ends with the suffix passed in the argument.
+# The return value is a boolean, and in this case it would return true.
 "Hello World".end_with?("rld")
 
-
+# The even? method is called on the integer object 12.
+# The even method does not receive any arguments
+# The return value is a boolean, and in this case it would return true.
 12.even?
 
-
+# The next method is called on the integer object 18.
+# The next method does not receive any arguments
+# The return value is the immediate successor of the number on which next is called.
+# In the following example next would return the integer 19.
 18.next
 
 
@@ -37,9 +51,26 @@
 # In this example, the return value is true, because "Jeff" does start with "J".
 # The puts command prints the return value of the start_with? method (true) to the console.
 first_name = "Jeff"
-puts first_name.start_with?("J")
+first_name.start_with?("J")
 
+# SECTION 2 Solution
 
+city = "Cleveland"
+mascot = "Browns"
+
+# The count() method is called on the city variable, which stores the string object "Cleveland".
+# The count() method returns an integer value representing the number of times the characters in the string argument
+# can be found in the object it is being called on (city => "Cleveland").
+# In this example, the return value is the integer 4, because there are 2 instances of "e" and two instances of "l" in the string "Cleveland".
+# The puts command prints the return value of the count() method to the console.
+puts city.count("el")
+
+# The upcase method is called on the mascot variable, which stores the string object "Browns".
+# The upcase method returns a new string with each character of the string object it is called on (mascot => "Browns") capitalized.
+# The method does not modify the original string whether it is a string literal or a string expression represented by a variable.
+# In this example, the return value is the string "BROWNS".
+# The puts command prints the return value of the upcase method to the console.
+puts mascot.upcase
 
 # SECTION 3: Calling methods on variables assigned to integers.
 # Declare 2 variables assigned to integer objects.
@@ -47,6 +78,21 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+year = 2009
+age = 28
+
+# The next method is called on the "year" variable, which stores an integer object with the value 2009.
+# The next method returns an integer that is the next highest value (i.e. <integer method called on> + 1) relative to the integer object on which it is called.
+# In the following scenario, the next method will return 2010, as that is the next highest integer after 2009.
+# Immediately after 2010 is returned, the puts method prints 2010 to the console.
+puts year.next
+
+# The even? method is called on the "age" variable, which stores an integer object with the value 28.
+# The even? method returns a boolean value. If the integer value on which it is called is an even integer, it will return true.
+# If the integer value on which even? is called is odd, the method will return false.
+# In this scenario, the even? method will return true since 28 is an even number.
+# The puts method will print the return value of true to the console.
+puts age.even?
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
@@ -54,3 +100,32 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+cities = ["Denver", "Cleveland", "Pittsburgh", "Chicago", "Cincinnati"]
+grades = [93, 82, 65, 49, 98, 76, 73, 91, 52, 88, 77, 93]
+# The each{} method is called on the variable named "cities", which is an array object that holds five strings (at declaration/assignment).
+# The each{} method is an enumerator function that iterates over each element of an arry one time.
+# The value returned by the each{} method is the original array on which the function was called.
+# If the each{} method is not given a code block after the "each" keyword, it will not throw an error.
+# Instead, it will return an Enumerator object.
+# If given a code block, the each{} function will perform the statements within the code on each value in the array ONE TIME (unless "return" keyword is 
+# included, which would cause the program to break out of the loop and continue executing the code immediately after the each{} method invocation).
+# In the following scenario, the each method is called followed by a "do...end" code block that tests each string in the array to see if it is longer than
+# 7 characters. If it is, the string is printed to the console using the puts keyword. The result of running the following code leads to three strings,
+# "Cleveland", "Pittsburgh", and "Cincinnati" being printed to the console since each of these strings is over 7 characters in length.
+
+cities.each do |city|
+    if city.length > 7
+        puts city
+    end
+end
+
+# The method sum is called on the variable "grades", which is an array of integer objects. As the name implies, the sum function adds each value in the array and
+# returns a new integer representing the sum of each integer value in the array. With JavaScript being the language with which I have the most experience, I thought
+# this method was fabulous. Much more code is needed for JavaScript with the most visually economical being the higher-order array function "reduce()". That Ruby
+# makes it as simple as calling .sum on the array is incredible. In the following scenario, the sum function called on the grades array will return the integer value 937.
+# After this value is returned, the puts function will print 937 to the console. I also added a second scenario since I can't believe how simple this code is to show how
+# easy it would be to get the average scores of a class, for instance.  grades.sum/grades.length immediately returns the average score of 78. In four words. I can't believe it.
+
+puts grades.sum
+puts grades.sum/grades.length
--- final_practice.rb
+++ final_practice.rb
@@ -4,45 +4,98 @@
 # Use the puts or print command to see the return value in the console.
 
 
-# 1: Write a method named greeting that returns a string with a general greeting. 
+# 1: Write a method named greeting that returns a string with a general greeting.
+def greeting(ref)
+    "Well hello there, #{ref}! How are you doing today?!?!"
+end
+partner = greeting("partner")
+guvnor = greeting("guv'nor")
+puts partner
+puts guvnor
 
 # What is the return value of your method?
+# A: The return value of the greeting method is a string object.
+
 # How many arguments did you pass your method?
+# A: I passed one argument to the greeting method.
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
+def custom_greeting(name, salutation)
+    "Howdy #{name}! #{salutation}"
+end
+
+cydnee = custom_greeting("Cydnee", "Great to see you again!!")
+kristen = custom_greeting("Kristen", "Hope you're having a fantastic day!!")
+puts cydnee
+puts kristen
 # What is the return value of your method?
+# A: The return value of custom_greeting is a string object.
 # How many arguments did you pass your method?
+# A: I passed two arguments to the custom_greeting method I wrote.
 # What data type was your argument(s)?
+# A: The data type for both arguments is string
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+def greet_person(f_name, m_name, l_name)
+    "Good day, #{f_name} #{m_name} #{l_name}!"
+end
+
+msd = greet_person("Matthew", "Shaw", "Darlington")
+knp = greet_person("Kristen", "Nicole", "Paullus")
+puts msd
+puts knp
 
 # What is the return value of your method?
+# A: The return value of greet_person is a string object
 # How many arguments did you pass your method?
+# A: I passed three arguments to my greet_person method
 # What data type was your argument(s)?
+# A: All three data types for the greet_person method are string objects
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
+def square(n)
+    "After processing, here is your returned square #{n**2}"
+end
+square_1 = square(3)
+square_2 = square(1200)
+puts square_1
+puts square_2
 
 # What is the return value of your method?
+# A: The return value of my square method is a string object.
 # How many arguments did you pass your method?
+# A: The square method I wrote only accepts one argument.
 # What data type was your argument(s)?
+# A: The data type of the argument for the square method is an integer object.
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
-
+def check_stock(qty, product)
+    def stock(q)
+        if q > 3
+            " is stocked"
+        elsif q <= 3 && q > 0
+            " - running LOW"
+        else
+            " - OUT of stock!"
+        end
+    end
+    puts "#{product}#{stock(qty)}"
+end
 check_stock(4, "Coffee");
-# => "Coffee is stocked"
+# # => "Coffee is stocked"
 
 check_stock(3, "Tortillas");
-# => "Tortillas - running LOW"
+# # => "Tortillas - running LOW"
 
 check_stock(0, "Cheese");
-# => "Cheese - OUT of stock!"
+# # => "Cheese - OUT of stock!"
 
 check_stock(1, "Salsa");
-# => "Salsa - running LOW"
+# # => "Salsa - running LOW"
